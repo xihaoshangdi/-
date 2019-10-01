daf890d880908562d45296c05c78a4d6647f3068
xihaoshangdi <xihaoshangdi@163.com>
2019-10-01 13:47:53 +0800

    重开了新的demo仓库

--- Html5 demo/LoadTrans/main.js
+++
@@ -1,8 +0,0 @@
-window.onload = function() {
-  var boxList = this.document.getElementsByClassName('box');
-  console.log(boxList);
-  var btn = this.document.getElementsByClassName('btn')[0];
-  btn.addEventListener('click', function() {
-    boxList[0].classList.add('demo');
-  });
-};
